04d57e0d3f870f66ffb10e3f42e23d254b669cf5
Airin <airin.eka.251@gmail.com>
2024-08-01 15:01:46 +0700

    first commit

---
+++ TokoSwing.java
@@ -0,0 +1,15 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+/**
+ * TokoSwing
+ */
+public class TokoSwing {
+
+    public static void main(String []args) {
+
+    }
+}
